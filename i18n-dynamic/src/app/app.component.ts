0724f0f9ebf3f8dde58d1097021b1fbf36316c33
Arnaud BODEL <contact@osw3.net>
2023-01-12 11:37:21 +0100

    -

--- i18n-dynamic/src/app/app.component.ts
+++ i18n-dynamic/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MessagesService } from './translation/services/messages.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'i18n-dynamic';
+  messages: {[key: string]: string} = {};
+
+  constructor(
+    private _messageService: MessagesService
+  ){}
+
+  ngOnInit(): void {
+    this._messageService.messages.subscribe( (data:{[key: string]: string}) => this.messages = data );
+  }
+
 }
