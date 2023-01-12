fbb3dc7158015dd824615636f0df08858e371e20
Arnaud BODEL <contact@osw3.net>
2023-01-12 09:42:38 +0100

    -

--- i18n/src/app/app.component.ts
+++ i18n/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'i18n';
+  title = "Jouons avec l'internationalisation";
 }
