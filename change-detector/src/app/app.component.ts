753f388f4e480563a2ae0891bbdd98a04684ad7d
Arnaud BODEL <contact@osw3.net>
2023-01-13 12:02:50 +0100

    -

--- change-detector/src/app/app.component.ts
+++ change-detector/src/app/app.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
+
+const getRandomInt = (max = 1000) => {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+
+  items: any[] = [...new Array(1000)].map((el, i) => ({id: i, value: getRandomInt()}));
+
+  update(id: number) {
+    const item = this.items[id];
+    item.value = getRandomInt();
+  }
 }
--- change-detector/src/app/item.component.ts
+++ change-detector/src/app/item.component.ts
@@ -3,7 +3,8 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 @Component({
   selector: 'app-item',
   template: `<li>{{item|json}}</li>`,
-  // changeDetection: ChangeDetectionStrategy.OnPush 
+  changeDetection: ChangeDetectionStrategy.OnPush 
+  // changeDetection: ChangeDetectionStrategy.Default 
 })
 export class ItemComponent {
   @Input() item: any;
